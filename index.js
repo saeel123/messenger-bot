ac8f1fae101c03d645893f511d9731c198ee7b8e
saeel123 <saeel@dccper.com>
2017-06-22 13:40:36 +0530

    reverted back to cc9c4efe01f26fcf22737ae1718a66c44c67f2ad

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-var mongoose = require("mongoose");
-
 
 const PORT = process.env.PORT || 5000;
-const db = mongoose.connect(process.env.MONGODB_URI);
-//const db = mongoose.connect("mongodb://localhost:27017/messenger-bot");
 
+var mongoose = require("mongoose");
 
-const webhooks = require('./routes/webhooks');
+var db = mongoose.connect(process.env.MONGODB_URI);
+var Movie = require("./movie.js");
 
-
+var Question = require("./models/question")
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-// Process application/json
-app.use(bodyParser.json());
 
-//middleware
-app.use('/webhook/', webhooks);
+// Process application/json
+app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
@@ -28,6 +24,185 @@ app.get('/', function (req, res) {
 })
 
 
+// Facebook Webhook
+// Used for verification
+app.get("/webhook/", function (req, res) {
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
+    }
+});
+
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function(entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+
+
+app.get("/test", function (req, res) {
+
+	let test = "";
+
+	res.send(test);
+});
+
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
+
+    if (payload === "Greeting") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function(error, response, body) {
+            var greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " +  error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            var message = greeting + "We are people who shape your story.";
+            sendMessage(senderId, {text: message});
+        });
+    } else if (payload === "Correct") {
+        sendMessage(senderId, {text: "Awesome! How we can help You"});
+    } else if (payload === "Incorrect") {
+        sendMessage(senderId, {text: "Oops! Sorry about that."});
+    }
+}
+
+function getMessengerName(senderId, callback) {
+
+	request({
+			url: "https://graph.facebook.com/v2.6/" + senderId,
+			qs: {
+					access_token: process.env.PAGE_ACCESS_TOKEN,
+					fields: "first_name"
+			},
+			method: "GET"
+	}, function(error, response, body) {
+			var greeting = "";
+			if (error) {
+					console.log("Error getting user's name: " +  error);
+			} else {
+					var bodyObj = JSON.parse(body);
+					name = bodyObj.first_name;
+			}
+
+			return callback(name);
+	});
+}
+
+function processMessage(event) {
+    if (!event.message.is_echo) {
+        var message = event.message;
+        var senderId = event.sender.id;
+
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
+
+        if (message.text) {
+						var formattedMsg = message.text.toLowerCase().trim();
+
+						if (formattedMsg === "hi" ) {
+
+							getMessengerName(senderId, function (res) {
+								sendMessage(senderId, {text: "Hi "+ res + ",How I can Help you?"});
+							});
+
+						} else {
+							Question.findByQuestion(formattedMsg, function (err, question) {
+
+								if (err) {
+									sendMessage(senderId, {text: "error occured while search"});
+								} else if (question === null){
+
+									let newQuestion = new Question({
+										question: formattedMsg
+									});
+
+									Question.addQuestion(newQuestion, function (newQuestion, err) {
+										if (!err) {
+											sendMessage(senderId, {text: "we will get back to you soon"});
+										} else {
+											sendMessage(senderId, {text: "We dint get what you want to say..."});
+										}
+									});
+								} else {
+									//sendMessage(senderId, {text: question['question']});
+									if (!question['answer']) {
+										sendMessage(senderId, {text: "We will get back to you..Thank you For you intrest"});
+									} else {
+										sendMessage(senderId, {text: question['answer']});
+									}
+								}
+							});
+						}
+        } else if (message.attachments) {
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+        }
+    }
+}
+
+
+function getQuestionAnswer(userId, question) {
+
+    Question.findByQuestion(question, function(err, question) {
+        if(err) {
+            sendMessage(userId, {text: "Something went wrong. Try again"});
+        } else {
+            sendMessage(userId, {text: question});
+        }
+    });
+
+}
+
+// sends message to user
+function sendMessage(recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
+}
+
+
+//const token = process.env.FB_PAGE_ACCESS_TOKEN;
 
 // Spin up the server
 app.listen(PORT, function() {
