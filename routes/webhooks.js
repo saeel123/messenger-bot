3dd9db71f05f95914d8d036e3538796119b638af
saeel123 <saeel@dccper.com>
2017-06-22 13:06:04 +0530

    code sepration updates

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -28,23 +28,26 @@ router.delete("/", function (req, res) {
 
 // All callbacks for Messenger will be POST-ed here
 router.post("/", function (req, res) {
-    // Make sure this is a page subscription
-    if (req.body.object == "page") {
-        // Iterate over each entry
-        // There may be multiple entries if batched
-        req.body.entry.forEach(function(entry) {
-            // Iterate over each messaging event
-            entry.messaging.forEach(function(event) {
-                if (event.postback) {
-                    processPostback(event);
-                } else if (event.message) {
-                    processMessage(event);
-                }
-            });
-        });
+    console.log("post");
+    res.send("post");
 
-        res.sendStatus(200);
-    }
+    // // Make sure this is a page subscription
+    // if (req.body.object == "page") {
+    //     // Iterate over each entry
+    //     // There may be multiple entries if batched
+    //     req.body.entry.forEach(function(entry) {
+    //         // Iterate over each messaging event
+    //         entry.messaging.forEach(function(event) {
+    //             if (event.postback) {
+    //                 processPostback(event);
+    //             } else if (event.message) {
+    //                 processMessage(event);
+    //             }
+    //         });
+    //     });
+    //
+    //     res.sendStatus(200);
+    // }
 });
 
 function processPostback(event) {
